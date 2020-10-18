ec920060b57939ff39f2b0b30ac0e48c327f0c14
ihsandroid1 <tombolspace@gmail.com>
2020-10-18 20:27:39 +0700

    fail to receive sms

---
+++ kirimsms.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid,authToken);
+
+client.messages.create({
+    to : process.env.MY_PHONE_NUMBER,
+    from : '+17205133851',
+    body : 'Hello hahamen'
+})
+.then((message) => console.log(message.sid))
